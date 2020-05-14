7d0e77e4a501feed61b9f73cbc99652343788154
Davids.berz <Davids.berz@gmail.com>
2020-05-14 08:47:23 -0500

    agregando anoatacion Enable feing client

--- src/main/java/com/mservice/app/alumnos/client/ICursoFeingClient.java
+++ src/main/java/com/mservice/app/alumnos/client/ICursoFeingClient.java
@@ -1,11 +1,10 @@
 package com.mservice.app.alumnos.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(value = "ms-cursos")
+@FeignClient(name = "ms-cursos")
 public interface ICursoFeingClient {
 
     @DeleteMapping("/eliminar-alumno/{id}")
